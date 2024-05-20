7dbdcb65de995d58d257fe80d956afe5a3125631
Seongha <sseongha11@gmail.com>
2024-05-20 23:53:08 +0100

    Initial commit

---
+++ src/test/java/com/lboro/ontology/OntologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lboro.ontology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
